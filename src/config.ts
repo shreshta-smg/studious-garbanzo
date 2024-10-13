c0f6c47f72fb6e580bb040e9d99227e8935c5ce4
shreshtadev <chinmai.bharadwaj@ymail.com>
2024-10-13 15:23:46 +0530

    Initial Commit V2

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,10 @@ import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
   website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  author: "shreshtadev",
+  profile: "https://shreshtasmg.in/",
+  desc: "",
+  title: "Shrehsta SMG",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
